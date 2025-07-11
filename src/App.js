f72858066b0793a30ec25653373a0f7a35878801
arnab-1 <arnab.vcc@gmail.com>
2025-07-11 23:33:49 +0530

    added onChange handelar and chatching the input field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 
 
 function App() {
+
+  const [title,setTitle] = useState('');
+  const [content,setContent] = useState('');
+
+  const fetch = () => {
+        console.log(title);
+        console.log(content);
+        
+        // will call backkend from here later
+      }
   return (
     <div className="App">
       <h3> blog app basic</h3>
-      <input placeholder="put Title here"/> 
-      <input placeholder="Put content here"/>
-      <button type="submit">create post</button>
+      <input 
+      placeholder="put Title here"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}/> 
+      <input 
+      placeholder="Put content here"
+      value={content}
+      onChange={(e) => setContent(e.target.value)}/>
+      <button type="submit" onClick={fetch}>create post</button>
 
     </div>
   );
